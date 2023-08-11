061e4c2fb3c969045aa965ee236d76e25c9a6acb
malhaouit <abdelmounaim.abdel@gmail.com>
2023-08-10 19:12:34 -0700

    Print diffrent sizes of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %d bytes(s)", sizeof(char));
-	printf("Size of an int: %d bytes(s)", sizeof(int));
-	printf("Size of a long: %d bytes(s)", sizeof(long int));
-	printf("Size of a long long int: %d bytes(s)", sizeof(long long int));
+	printf("Size of char: %d bytes(s)\n", sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long: %d bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %d bytes(s)", sizeof(float));
 	return (0);
 }
