395a86b9595656e3df2f51d57939e35912e1bdca
malhaouit <abdelmounaim.abdel@gmail.com>
2023-08-15 18:40:27 -0700

    Return 1 if charachter is lowercase or 0 if something else

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - function that  checks if character c is lowercase
+ * @c: the character to be checked
+ *
+ * Return: 1 if lowercase or 0 if something else
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+
