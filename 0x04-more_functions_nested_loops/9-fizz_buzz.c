5782ec8774ef5cd7b41a3198181936d7b25c2f88
malhaouit <abdelmounaim.abdel@gmail.com>
2023-08-17 17:42:10 -0700

    Fizz, Buzz or FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,21 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else
 		{
 			if (i % 3 == 0)
-				printf("Fizz ");
+				printf(" Fizz");
 			if (i % 5 == 0)
-				printf("Buzz ");
+				printf(" Buzz");
 			if ((i % 3 != 0) && (i % 5 != 0))
-				printf("%d ", i);
+			{
+				if (i != 1)
+					printf(" %d", i);
+				else
+					printf("%d", i);
+			}
 		}
 	}
 	printf("\n");
