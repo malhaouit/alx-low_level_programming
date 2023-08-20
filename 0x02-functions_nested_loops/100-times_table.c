470e08db8aa87cde389e38dc7c3f7f7b53f65cbb
malhaouit <abdelmounaim.abdel@gmail.com>
2023-08-19 17:28:31 -0700

    Print n times table between 0 and 15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * print_times_table - print times table between 0 and 15
+ * @n: number of times table
+ * Return: nothing
+ */
+void print_times_table(int n)
+{
+	if (n >= 0 && n <= 15)
+	{
+		int i, j, k;
+
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+
+				if (j == 0)
+				{
+					_putchar(k + '0');
+					_putchar(',');
+				}
+				else
+				{
+					if (k <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(k + '0');
+					}
+					if (k > 9 && k < 100)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(k / 10 + '0');
+						_putchar(k % 10 + '0');
+					}
+					if (k >= 100)
+					{
+						_putchar(' ');
+						_putchar(k / 100 + '0');
+						_putchar((k / 100) % 10 + '0');
+						_putchar(k % 10 + '0');
+					}
+					if (j != n)
+						_putchar(',');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
