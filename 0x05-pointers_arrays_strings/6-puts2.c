6f1112092a3644e0856387e17515ff90ab04ed0b
malhaouit <abdelmounaim.abdel@gmail.com>
2023-08-21 14:26:03 -0700

    Prints the other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints the other character of string starting with 0
+ * @str: the string
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
