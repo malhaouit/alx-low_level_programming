61f3928b43be7a0cab6140cc5273fcba43558efc
malhaouit <abdelmounaim.abdel@gmail.com>
2023-08-27 05:02:52 -0700

    Prints an integer using _putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,22 @@
  */
 void print_number(int n)
 {
+	int numberOfDigits;
+	int temporary;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-
-	int numberOfDigits = 0;
-	int temporary = n;
+	numberOfDigits = 0;
+	temporary = n;
 
 	while (temporary > 0)
 	{
@@ -27,12 +35,13 @@ void print_number(int n)
 	{
 		int divisor = 1;
 		int i;
+		int digit;
 
 		for (i = 1; i < numberOfDigits; i++)
 		{
 			divisor *= 10;
 		}
-		int digit = (n / divisor) 10;
+		digit = (n / divisor) % 10;
 
 		_putchar(digit + '0');
 		numberOfDigits--;
