2626a01563b5ecdc07552d7bc0156fd7b2fa5004
malhaouit <abdelmounaim.abdel@gmail.com>
2023-08-28 12:41:07 -0700

    Copy n bytes from memory address to destination memory address

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copy n bytes from memory area src to memory
+ * area dest
+ * @dest: The destination memory address
+ * @src: Te source memory address
+ * @n: Number of bytes to be copied
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	return (dest);
+
