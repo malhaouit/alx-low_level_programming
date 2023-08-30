c12d46e05876036842fa39f8f09521ed1055f941
malhaouit <abdelmounaim.abdel@gmail.com>
2023-08-30 14:29:38 -0700

    Prints in reverse recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -14,8 +14,9 @@ void _print_rev_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-	
-	_print_rev_recursion(s++);
-
-	_putchar(*s);
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
