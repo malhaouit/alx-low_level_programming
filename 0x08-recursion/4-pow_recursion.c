5ddce49906adb16215fb0e71f00262928e4aa181
malhaouit <abdelmounaim.abdel@gmail.com>
2023-08-30 15:46:50 -0700

    Calculate power recursion of a give integer

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Calculate the power of a given number
+ * @x: The number that we apply to it the power
+ * @y: The power to a number
+ *
+ * Return: Result of power of x if x is positive, or -1 if negative
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
