0900027e24ccf00f578d8aa5b2abc9a3a9f52d47
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-01 14:12:23 -0700

    Create static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 
-/**
- * _puts - prints a string followed by a new line to stdout
- * @str: the string
- * Return: nouthing
- */
 void _puts(char *str)
 {
 	puts(str);
