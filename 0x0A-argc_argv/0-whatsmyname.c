92872508f9f17832882fabada74cc676707ab558
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-01 17:36:32 -0700

    Prints the name of program itself

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program itself
+ * @argc: The size of array argv
+ * @argv: The array
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
