db18b8f3f3da4ad0067a378b214602e8df819cc3
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-01 17:46:04 -0700

    Prints the number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments
+ * @argc: The size of array containing the arguments
+ * @argv: The array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
