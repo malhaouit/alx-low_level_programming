71bca8aee9596c5cdffa2b8b33a15d21f6af8b24
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-04 13:47:46 -0700

    Create a function that point to duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a duplicate string
+ * @str: The source string to duplicate
+ * Return: NULL if sr is NULL or it fails, or return
+ * a pointer to the duplicate string if success
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	char *temp = str;
+	int length = 0;
+	int i;
+
+	if (*str == '\0')
+		return (NULL);
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+	ptr = malloc(sizeof(char) * (length + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < length; i++)
+		ptr[i] = temp[i];
+	ptr[length] = '\0';
+
+	return (ptr);
+}
