adcff5a309be67a52b22326d01dbfd3a9152a4a5
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-07 11:23:15 -0700

    Allocate memory for an array using calloc prototype

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int total_size;
 	char *ptr;
-	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -24,9 +23,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
-
-	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
 
 	return (ptr);
 }
