c9d1cbc7a2d2607a2467517bebd601c84b1e8f58
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-07 11:08:50 -0700

    Allocates memory for an array using malloc prototype

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array
+ * @nmemb: Number of elements
+ * @size: Size of every elements in bytes
+ *
+ * Return: Pointer to the momery allocated, or NULL if something
+ * fails or nmemb or size equal to 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int total_size;
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total_size = nmemb * size;
+
+	ptr = malloc(total_size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nemmb; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
