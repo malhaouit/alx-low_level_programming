44b94438f005c70702e14aa2f31599d7e0afa694
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-11 17:54:14 -0700

    Define a struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Defines a sturcture for dog information.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The name of the dog's owner
+ *
+ * Description: This sturct includes the basic attributes of a dog,
+ * including its name, age and owner.
+ */
+
 struct dog
 {
 	char *name;
