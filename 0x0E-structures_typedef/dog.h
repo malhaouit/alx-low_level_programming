696161e0be9f2828d96a92ac82196c62aae41fae
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-11 18:51:58 -0700

    Define a new name of struct name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,12 +11,12 @@
  * including its name, age and owner.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
