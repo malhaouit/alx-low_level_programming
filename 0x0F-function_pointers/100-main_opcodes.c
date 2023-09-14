780621d1253810affaeb10fdf997c44a9cfc2ad0
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-13 19:47:20 -0700

    Update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -30,7 +30,11 @@ int main(int argc, char *argv[])
 	}
 
 	for (i = 0; i < num_bytes; i++)
-		printf("%02x ", main_ptr[i]);
+	{
+		printf("%02x", main_ptr[i]);
+		if (i != num_bytes - 1)
+			printf(" ");
+	}
 
 	printf("\n");
 
