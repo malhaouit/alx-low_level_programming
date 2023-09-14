8bc418957b3c76aacc998396d3224862b20cfef4
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-14 16:33:52 -0700

    Update file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,11 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	int num;
+	va_list args;
 
 	if (separator == NULL)
 		return;
-
-	va_list args;
 
 	va_start(args, n);
 
