4dd7bf33116703364f62107f02434f9e839d6939
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-22 15:54:34 -0700

    Free a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Free all the memory allocated for the linked list.
+ * @head: Pointer tro the first element.
+ *
+ * Return: Nothing
+ */
+void free_list(lsit_t *head)
+{
+	list_t *current = head;
+	list_t *new;
+
+	while (current != NULL)
+	{
+		new = current->next;
+		free(current->str);
+		free(current);
+		current = new;
+	}
+}
