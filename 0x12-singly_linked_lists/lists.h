40aa80e7bd4aae1b3e7693df080e5da9a4cae318
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-22 12:14:13 -0700

    Update header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,5 +20,7 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+unsigned int find_length(const char *str);
 
 #endif /* LISTS_H */
