b01065909be2aa509d87f650a5135952d4b76188
malhaouit <abdelmounaim.abdel@gmail.com>
2023-09-25 14:08:53 -0700

    Update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,17 +9,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
 	if (head == NULL || *head == NULL)
 		return;
 
-	while ((*head)->next != NULL)
+	while (*head != NULL)
 	{
+		temp = *head;
 		*head = (*head)->next;
 		free(temp);
-		temp = *head;
 	}
-	*head = NULL;
-	free(temp);
 }
