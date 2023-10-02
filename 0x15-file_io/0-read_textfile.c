27a6c0809e384f07d22ebcb368831d3157bf2709
malhaouit <abdelmounaim.abdel@gmail.com>
2023-10-02 13:31:06 -0700

    uPDATE

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -30,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1)
+	if (bytes_written == -1 || bytes_written != bytes_read)
 		return (0);
 
 	close(fd);
