1b14405c5e2486247c75400978347025fae813d1
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-09 13:03:23 -0800

    Add the function description

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * dlistint_len - Finds the length of a doubly linked list
+ * @h: A pointer to the first node
+ *
+ * Return: The length of the linked list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t len = 0;
