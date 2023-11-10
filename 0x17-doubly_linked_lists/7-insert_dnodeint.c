0cafe1a8e367a7a220b7423891a0c9bf932114a2
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-09 16:39:36 -0800

    Insert a node at specific index in a doubly link list

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,71 @@
+#include "lists.h"
+#include <stdlib.h>
+
+
+/**
+ * dlistint_len - Finds the length of a doubly linked list
+ * @h: A pointer to the first node
+ *
+ * Return: The length of the linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		len++;
+		current = current->next;
+	}
+
+	return (len);
+}
+
+/**
+ * *insert_dnodeint_at_index - Insert a node at a specific index
+ * @h: A pointer to a pointer to the first node of the list
+ * @idx: The index where the new node will be added
+ * @n: Int data to store in the node to be added
+ *
+ * Return: New node if success, or NULL otherwise
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *current;
+	size_t current_index = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new || idx > dlistint_len(*h))
+		return (NULL);
+
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+	else
+	{
+		current = *h;
+		while (current != NULL)
+		{
+			if (current_index == idx)
+			{
+				new->n = n;
+				new->next = current;
+				new->prev = current->prev;
+				current->prev = new;
+				new->prev->next = new;
+				return (new);
+			}
+			else if (current->next == NULL)
+			{
+				new = add_dnodeint_end(h, n);
+				return (new);
+			}
+			current_index++;
+			current = current->next;
+		}
+	}
+	return (NULL);
+}
