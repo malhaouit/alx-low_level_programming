8a6accd614cf3cbd95a0b5c7b5c09c6339d03836
malhaouit <abdelmounaim.abdel@gmail.com>
2023-11-13 14:14:06 -0800

    Update: Add the link to download the libgm.so from github to the directory /link on the server

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #/bin/bash
-LD_PRELOAD=$(pwd)/libgm.so ./gm $1 $2 $3 $4 $5 $6
+wget -P /link https://github.com/malhaouit/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgm.so
+LD_PRELOAD=$(pwd)/link/libgm.so ./gm $1 $2 $3 $4 $5 $6
