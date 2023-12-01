b0231a1f2896bbce75cf7176c76a4a0ed36085e5
malhaouit <abdelmounaim.abdel@gmail.com>
2023-12-01 00:59:03 -0800

    Gets a value from the hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_get - Gets a value from the hash table
+ * @ht: A pointer to the hash table
+ * @key: The string key used to find the value
+ *
+ * Return: The value if found, or NULL if not
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+
+	return (NULL);
+}
